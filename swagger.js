83f17100a6f991df9ef74a336783cbf4c3f7add3
OMER RAAD <omerraad444@gmail.com>
2024-05-02 01:55:53 +0300

    changes on swagger-output

--- swagger.js
+++ swagger.js
@@ -5,9 +5,7 @@ const doc = {
     title: 'API FOR HOSPITAL INFORMATION SYSTEM',
     description: 'API for Hospital Information System. The HIS API provides a secure and efficient interface for managing hospital operations, including patient data, appointment scheduling'
   },
-  // Remove the scheme from the host
   host: 'his-50kt.onrender.com',
-  // Specify the schemes separately
   schemes: ['https'], // or ['http']
 };
 
