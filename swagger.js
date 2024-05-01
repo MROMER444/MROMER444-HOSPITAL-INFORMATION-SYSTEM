4393ee652b21d4d120c89c8b654d6ac7b4d8575a
OMER RAAD <omerraad444@gmail.com>
2024-05-02 01:43:46 +0300

    add https to swagger

--- swagger.js
+++ swagger.js
@@ -3,9 +3,12 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'API FOR HOSPITAL INFORMATION SYSTEM',
-    description: 'api for Hospital Information SystemThe HIS API provides a secure and efficient interface for managing hospital operations, including patient data, appointment scheduling'
+    description: 'API for Hospital Information System. The HIS API provides a secure and efficient interface for managing hospital operations, including patient data, appointment scheduling'
   },
-  host: 'https://his-50kt.onrender.com',
+  // Remove the scheme from the host
+  host: 'his-50kt.onrender.com',
+  // Specify the schemes separately
+  schemes: ['https'], // or ['http']
 };
 
 const outputFile = './swagger-output.json';
@@ -13,4 +16,4 @@ const endpointsFiles = ['./index.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
     require('./index')
-})
+});
